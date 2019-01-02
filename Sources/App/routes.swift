f4e84466ae58f17b813376a141097bd2b7135575
bluenano <sean.schlaefli@gmail.com>
2019-01-01 16:49:20 -0800

    Fixed typo in routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,7 @@ public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
         return "It works!"
-    }gi
+    }
     
     // Configure golf courses controller
     let golfCoursesController = GolfCoursesController()
